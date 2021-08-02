08ba50bfd5a2b98777ad436f7d4a9069d8a45033
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-02 22:07:56 +0300

    react router dom and axios

---
+++ fetching/src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from "./components/users";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about" component={About}>
+            <About />
+          </Route>
+          <Route path="/users" component={Users}>
+            <Users />
+          </Route>
+          <Route path="/" component={Home}>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+
+export default App;
---
+++ fetching/src/components/users.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Users() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    axios("https://jsonplaceholder.typicode.com/users")
+      .then((data) => {
+        setUsers(data.data);
+      })
+      .catch((e) => console.log(e))
+      .finally(() => setIsLoading(false));
+  }, []);
+  return (
+    <div>
+      <h1>Users</h1>
+      {isLoading && <div>Loading...</div>}
+      {users.map((user) => (
+        <div key={user.id}>{user.name}</div>
+      ))}
+    </div>
+  );
+}
