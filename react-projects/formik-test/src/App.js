cefdba4d8cb263ec119a93781af6b1db68c29a4b
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-04 01:26:15 +0300

    formik içinde kendi inputlarını kullanma

--- react-projects/formik-test/src/App.js
+++ react-projects/formik-test/src/App.js
@@ -1,4 +1,4 @@
-import { Field, Form, Formik } from "formik";
+import { Formik } from "formik";
 import React from "react";
 
 export default function App() {
@@ -12,30 +12,43 @@ export default function App() {
           email: "",
         }}
         onSubmit={(values) => {
-          alert(values);
+          console.log(values);
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="Jane" />
-          <br />
-          <br />
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              id="firstName"
+              onChange={handleChange}
+              name="firstName"
+              placeholder="Jane"
+            />
+            <br />
+            <br />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
-          <br></br>
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              id="lastName"
+              onChange={handleChange}
+              name="lastName"
+              placeholder="Doe"
+            />
+            <br></br>
 
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="jane@acme.com"
-            type="email"
-          />
-          <br></br>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              placeholder="jane@acme.com"
+              type="email"
+              onChange={handleChange}
+            />
+            <br></br>
 
-          <button type="submit">Submit</button>
-        </Form>
+            <button type="submit">Submit</button>
+          </form>
+        )}
       </Formik>
     </div>
   );
