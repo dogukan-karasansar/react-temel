b0d6ccd577571e4331acd0cff7bdcbedbe8327d5
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-06 17:35:01 +0300

    react memo

---
+++ usememo/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import Header from "./components/Header";
+
+function App() {
+  const [number, setNumber] = useState(0);
+  return (
+    <div className="App">
+      <Header number={number < 5 ? 0 : number} />
+      <hr />
+      <div>{number}</div>
+      <button onClick={() => setNumber(number + 1)}>Click</button>
+    </div>
+  );
+}
+
+export default App;
