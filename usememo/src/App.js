f467d295ea4fea58652b3fe0c062ccd7af18d325
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-08 20:26:17 +0300

    useMemo

--- usememo/src/App.js
+++ usememo/src/App.js
@@ -1,18 +1,27 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import Header from "./components/Header";
 
 function App() {
   const [number, setNumber] = useState(0);
+  const data = useMemo(() => {
+    return CalculateObject();
+  }, []);
   return (
     <div className="App">
-      <Header number={number < 5 ? 0 : number} />
+      <Header data={data} />
       <hr />
       <div>{number}</div>
       <button onClick={() => setNumber(number + 1)}>Click</button>
     </div>
   );
+}
+
+function CalculateObject() {
+  console.log("calculating");
+  for (let i = 0; i <= 100000000; i++) {}
+  console.log("calculating completed");
+  return { name: "mehmet" };
 }
 
 export default App;
--- usememo/src/components/Header.js
+++ usememo/src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
 
-function Header({ number }) {
+function Header({ number, data }) {
   console.log("Header Mounted");
-  return <div>Header - {number}</div>;
+  return (
+    <div>
+      Header - {number}
+      <br />
+      <code>{JSON.stringify(data)}</code>
+    </div>
+  );
 }
 
 export default React.memo(Header);
