f6281fa5f90e315b048f601b58fa67a7bcff9655
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-21 21:56:25 +0300

    default locale

--- react-intl/src/App.js
+++ react-intl/src/App.js
@@ -1,29 +1,34 @@
-import { useState } from "react";
-import { IntlProvider, FormattedMessage, FormattedNumber } from "react-intl";
+import { useEffect, useState } from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
 import "./App.css";
 
 function App() {
+  const defaultLocale = localStorage.getItem("localKey") ?  localStorage.getItem("localKey") :  navigator.language;
+  console.log(defaultLocale);
   const messages = {
     "tr-TR": {
       title: "Merhaba Dünya",
       description: "yeni mesajlarınız geldi",
     },
-    "en-EN": {
+    "en-US": {
       title: "Hello World",
       description: "you have new messages",
     },
   };
-  const [lang, setLang] = useState("tr-TR");
+  const [locale, setLocale] = useState(defaultLocale);
+  useEffect(() => {
+    localStorage.setItem("localKey", locale);
+  }, [locale]);
   return (
     <div className="App">
-      <IntlProvider messages={messages[lang]}>
+      <IntlProvider messages={messages[locale]}>
         <FormattedMessage id="title" />
         <br />
         <FormattedMessage id="description" />
         <br />
         <br />
-        <button onClick={() => setLang("tr-TR")}>TR</button>
-        <button onClick={() => setLang("en-EN")}>EN</button>
+        <button onClick={() => setLocale("tr-TR")}>TR</button>
+        <button onClick={() => setLocale("en-US")}>EN</button>
       </IntlProvider>
     </div>
   );
