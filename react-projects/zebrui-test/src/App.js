8bbed27c3b56be0c265f555f18253724e88817ac
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-08-25 21:33:59 +0300

    lobus ui tested

--- react-projects/zebrui-test/src/App.js
+++ react-projects/zebrui-test/src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
-import { Button, Paragraph } from "zebrui";
-import "../node_modules/zebrui/dist/index.css";
+/* import { Button, Paragraph } from "zebrui"; */
+import { Button } from "lobus-button-ui";
+/* import "../node_modules/zebrui/dist/index.css"; */
 
 function App() {
   return (
     <div className="App">
-      <Button text={"Ödeme"} />
-      <Paragraph text={"cafdsfsdfsdfsdƒ"} />
+      {/* <Button text={"Ödeme"} />
+      <Paragraph text={"cafdsfsdfsdfsdƒ"} /> */}
+      <Button text={"Clicked"} type={"primary"} />
     </div>
   );
 }
