9f1d8192e6ed66c3847f38bf0a6ca9eca7a1b68a
karasansar <55123100+dogukan-karasansar@users.noreply.github.com>
2021-10-03 17:27:10 +0300

    detail page

--- redux/breaking-bad-app/src/pages/detail/detail.jsx
+++ redux/breaking-bad-app/src/pages/detail/detail.jsx
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router";
+import axios from "axios";
 
 export default function Detail() {
-  return <div>Details page</div>;
+  const [char, setChar] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const { char_id } = useParams();
+  useEffect(() => {
+    axios(`${process.env.REACT_APP_BASE_ENDPOINT}/characters/${char_id}`)
+      .then((res) => res.data)
+      .then((data) => setChar(data[0]))
+      .finally(() => setLoading(false));
+  }, [char_id]);
+
+  return (
+    <div>
+      {loading && <div>Loading...</div>}
+      {char && (
+        <>
+          <div>
+            <h1>{char.name}</h1>
+            <img src={char.img} style={{ width: "50%" }} alt={char.name}/>
+          </div>
+        </>
+      )}
+      {char && <pre>{JSON.stringify(char, null, 2)}</pre>}
+    </div>
+  );
 }
--- redux/breaking-bad-app/src/pages/home/index.jsx
+++ redux/breaking-bad-app/src/pages/home/index.jsx
@@ -31,7 +31,7 @@ export default function Home() {
       >
         {characters.map((character) => (
           <div key={character.char_id}>
-            <Link to="/char/2">
+            <Link to={`/char/${character.char_id}`}>
               <img
                 alt={character.name}
                 src={character.img}
